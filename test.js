a5182c16a1da8acbabdf8a603a547ea8c4491b20
Mahdi Shadkamfarrokhi <ms6920@nyu.edu>
2016-08-06 16:29:43 -0400

    meh

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
+//TO DO :
+//Setup the input fields
+//Setup defaults
+//have page load properly on Button Click
+
 function compile(){
 	$("p").load("The New York Times - Breaking News, World News & Multimedia.html");
 }
+
+
